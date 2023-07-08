3106b1b266f63d9c93735a36112adbf84918e4e9
QuirkyLarry <larrytmoore1@gmail.com>
2023-07-08 10:30:23 -0500

    Update 3
    Added Scale Slider
    Added toggle to enable/disable.
    Changed to more efficient render method.
    Confirmed Location(x,y) sliders work.
    Hud & Text now scales with screen.

--- lua/autorun/client/propcounterv3.lua
+++ lua/autorun/client/propcounterv3.lua
@@ -1,66 +1,39 @@
-local l_cACCk = cvars.AddChangeCallback
-local l_CCCVr = (CLIENT && CreateClientConVar or NULL)
-local l_Cr = (CLIENT && Color or NULL)
-local l_sCFt = (CLIENT && surface.CreateFont or NULL)
-local l_hAd = (CLIENT && hook.Add or NULL)
-local l_GCVr = (CLIENT && GetConVar or NULL)
-local l_dRBEx = (CLIENT && draw.RoundedBoxEx or NULL)
-local l_dSTt = (CLIENT && draw.SimpleText or NULL)
-local l_LP = (CLIENT && LocalPlayer or NULL)
-l_CCCVr("lpc_HClr_r", 0, true, false, "", 0, 255 )
-l_CCCVr("lpc_HClr_g", 191, true, false, "", 0, 255 )
-l_CCCVr("lpc_HClr_b", 255, true, false, "", 0, 255 )
-l_CCCVr("lpc_HClr_a", 255, true, false, "", 0, 255 )
-l_CCCVr("lpc_BGClr_r", 255, true, false, "", 0, 255 )
-l_CCCVr("lpc_BGClr_g", 255, true, false, "", 0, 255 )
-l_CCCVr("lpc_BGClr_b", 255, true, false, "", 0, 255 )
-l_CCCVr("lpc_BGClr_a", 255, true, false, "", 0, 255 )
-l_CCCVr("lpc_HTClr_r", 255, true, false, "", 0, 255 )
-l_CCCVr("lpc_HTClr_g", 255, true, false, "", 0, 255 )
-l_CCCVr("lpc_HTClr_b", 255, true, false, "", 0, 255 )
-l_CCCVr("lpc_HTClr_a", 255, true, false, "", 0, 255 )
-l_CCCVr("lpc_BGTClr_r", 0, true, false, "", 0, 255 )
-l_CCCVr("lpc_BGTClr_g", 191, true, false, "", 0, 255 )
-l_CCCVr("lpc_BGTClr_b", 255, true, false, "", 0, 255 )
-l_CCCVr("lpc_BGTClr_a", 255, true, false, "", 0, 255 )
-l_CCCVr("lpc_vertical", ScrH() * .02, true, false, "", 10, ScrH()- 10 )
-l_CCCVr("lpc_horizontal", ScrW() * .010, true, false, "", 10, ScrW()- 10 )
-l_CCCVr("lpc_HH", ScrH() * .020, true, false, "", 10, ScrH()- 10 )
-l_CCCVr("lpc_HW", ScrW() * .05, true, false, "", 10, ScrW()- 10 )
-l_CCCVr("lpc_BGH", ScrH() * .030, true, false, "", 10, ScrH()- 10 )
-l_CCCVr("lpc_BGW", ScrW() * .05, true, false, "", 10, ScrW()- 10 )
---[[
-local propCounterHeadHeight = ScrH() * .020
-local propCounterHeadWidth = ScrW() * .05
-local propCounterHeight = ScrH() * .030
-local propCounterWidth = ScrW() * .05
-]]
--- Grabs Screen Width and Height and stores as variable.
-local ScW = ScrW()
-local ScH = ScrH()
+CreateClientConVar("lpc_HClr_r", 0, true, false, "", 0, 255 )
+CreateClientConVar("lpc_HClr_g", 191, true, false, "", 0, 255 )
+CreateClientConVar("lpc_HClr_b", 255, true, false, "", 0, 255 )
+CreateClientConVar("lpc_HClr_a", 255, true, false, "", 0, 255 )
+CreateClientConVar("lpc_BGClr_r", 255, true, false, "", 0, 255 )
+CreateClientConVar("lpc_BGClr_g", 255, true, false, "", 0, 255 )
+CreateClientConVar("lpc_BGClr_b", 255, true, false, "", 0, 255 )
+CreateClientConVar("lpc_BGClr_a", 255, true, false, "", 0, 255 )
+CreateClientConVar("lpc_HTClr_r", 255, true, false, "", 0, 255 )
+CreateClientConVar("lpc_HTClr_g", 255, true, false, "", 0, 255 )
+CreateClientConVar("lpc_HTClr_b", 255, true, false, "", 0, 255 )
+CreateClientConVar("lpc_HTClr_a", 255, true, false, "", 0, 255 )
+CreateClientConVar("lpc_BGTClr_r", 0, true, false, "", 0, 255 )
+CreateClientConVar("lpc_BGTClr_g", 191, true, false, "", 0, 255 )
+CreateClientConVar("lpc_BGTClr_b", 255, true, false, "", 0, 255 )
+CreateClientConVar("lpc_BGTClr_a", 255, true, false, "", 0, 255 )
+CreateClientConVar("lpc_vertical", 10, true, false, "", 1, ScrH())
+CreateClientConVar("lpc_horizontal", 10, true, false, "", 1, ScrW())
+CreateClientConVar("lpc_scale", 1, true, false, "", 0, 10)
+CreateClientConVar("lpc_enabled", 1, true, false, "", 0, 1)
+local scale = GetConVar("lpc_scale"):GetInt()
+local ScW, ScH = ScrW(),ScrH()
+local vLocation, hLocation = GetConVar("lpc_vertical"):GetInt(),GetConVar("lpc_horizontal"):GetInt()
+local hr,hg,hb,ha = GetConVar("lpc_HClr_r"):GetInt(),GetConVar("lpc_HClr_g"):GetInt(),GetConVar("lpc_HClr_b"):GetInt(),GetConVar("lpc_HClr_a"):GetInt()
+local htr,htg,htb,hta = GetConVar("lpc_HTClr_r"):GetInt(),GetConVar("lpc_HTClr_g"):GetInt(),GetConVar("lpc_HTClr_b"):GetInt(),GetConVar("lpc_HTClr_a"):GetInt()
+local bgr,bgg,bgb,bga = GetConVar("lpc_BGClr_r"):GetInt(),GetConVar("lpc_BGClr_g"):GetInt(),GetConVar("lpc_BGClr_b"):GetInt(),GetConVar("lpc_BGClr_a"):GetInt()
+local bgtr,bgtg,bgtb,bgta = GetConVar("lpc_BGTClr_r"):GetInt(),GetConVar("lpc_BGTClr_g"):GetInt(),GetConVar("lpc_BGTClr_b"):GetInt(),GetConVar("lpc_BGTClr_a"):GetInt()
+local FontSize = (((ScH *.03)+(ScW *.04))/4) * scale
 
-local pcLocation = {["v"] = l_GCVr("lpc_vertical"):GetInt(), ["h"] =  l_GCVr("lpc_horizontal"):GetInt()}
-local pcSize = {['HH'] = l_GCVr("lpc_HH"):GetInt(),['HW'] = l_GCVr("lpc_HW"):GetInt(),['BGH'] = l_GCVr("lpc_BGH"):GetInt(),['BGW'] = l_GCVr("lpc_BGW"):GetInt()}
-local pcHClrT = {['r'] = l_GCVr("lpc_HClr_r"):GetInt(),['g'] = l_GCVr("lpc_HClr_g"):GetInt(),['b'] = l_GCVr("lpc_HClr_b"):GetInt(),['a'] = l_GCVr("lpc_HClr_a"):GetInt()}
-local pcHTClrT = {['r'] = l_GCVr("lpc_HTClr_r"):GetInt(),['g'] = l_GCVr("lpc_HTClr_g"):GetInt(),['b'] = l_GCVr("lpc_HTClr_b"):GetInt(),['a'] = l_GCVr("lpc_HTClr_a"):GetInt()}
-local pcBGClrT = {['r'] = l_GCVr("lpc_BGClr_r"):GetInt(),['g'] = l_GCVr("lpc_BGClr_g"):GetInt(),['b'] = l_GCVr("lpc_BGClr_b"):GetInt(),['a'] = l_GCVr("lpc_BGClr_a"):GetInt()}
-local pcBGTClrT = {['r'] = l_GCVr("lpc_BGTClr_r"):GetInt(),['g'] = l_GCVr("lpc_BGTClr_g"):GetInt(),['b'] = l_GCVr("lpc_BGTClr_b"):GetInt(),['a'] = l_GCVr("lpc_BGTClr_a"):GetInt()}
-
-l_sCFt( "PropCounterHeadText", {
+surface.CreateFont( "PropCounterText", {
 	font = "Arial",
 	extended = false,
-	size = ScrW() * .010,
-	weight = 650,
+	size = (((ScH *.03)+(ScW *.04))/4) * scale,
+	weight = 600,
 	antialias = true,
-  })
-  
-  l_sCFt( "PropCounterText", {
-	font = "Arial",
-	extended = false,
-	size = ScrW() * .020,
-	weight = 1000,
-	antialias = true,
-  })
+})
 
 -- Creates Larry's Prop Counter Category
 hook.Add( "AddToolMenuCategories", "PropCounterCategory", function()
@@ -75,81 +48,88 @@ hook.Add( "PopulateToolMenu", "PropCounterSettings", function()
 		ColorWheelPanel:ColorPicker("Count Text Color", "lpc_BGTClr_r", "lpc_BGTClr_g", "lpc_BGTClr_b", "lpc_BGTClr_a" )
 	end )
 	spawnmenu.AddToolMenuOption( "Utilities", "#Larry's Prop Counter", "PCLM", "#Prop Counter Settings", "", "", function(LPH)
-		LPH:NumSlider( "Vertical Location Slider", "lpc_vertical", 10,ScH - (pcSize['HH']+ pcSize['BGH'] + 10), 0 )
-		LPH:NumSlider( "Horizontal Location Slider", "lpc_horizontal", 10,ScW - (pcSize['HW'] + 10), 0 )
+		LPH:NumSlider( "Vertical Location Slider", "lpc_vertical", 1,ScH- 1, 0 )
+		LPH:NumSlider( "Horizontal Location Slider", "lpc_horizontal", 1,ScW-1, 0 )
+		LPH:NumSlider( "Hud Scale", "lpc_scale", 0,10, 2 )
+		LPH:CheckBox( "Toggle Prop Counter", "lpc_enabled" )
 	end )
 end )
 
+local pcHUDH = function()
+	surface.SetDrawColor(hr,hg,hb,ha)
+	surface.DrawRect(hLocation, vLocation, FontSize*3, FontSize)
+	surface.SetTextColor(htr,htg,htb)
+	surface.SetTextPos(hLocation+(ScH/ScW), vLocation)
+	surface.SetFont( "PropCounterText" )
+	surface.DrawText( "Props:" )
+end
 
+local pcHUDBG = function()
+	surface.SetDrawColor(bgr, bgg, bgb, bga )
+	surface.DrawRect(hLocation+(FontSize*3), vLocation, FontSize*1.75, FontSize)
+	surface.SetTextColor(bgtr, bgtg, bgtb)
+	surface.SetTextPos((hLocation+(FontSize*3)), vLocation)
+	surface.SetFont( "PropCounterText" )
+	surface.DrawText( LocalPlayer():GetCount( "props" ) or 0 )
+end
 
+hook.Add("HUDPaint", "ClientPropCounterHUDHook", function(ScW,ScH) pcHUDH() pcHUDBG() end)
 
-l_hAd("HUDPaint", "ClientPropCounterHUDHook", function()
-	-- Top Color Bar - Says "Prop Count"
-	l_dRBEx(3,pcLocation["h"], pcLocation["v"], pcSize['HW'], pcSize['HH'], l_Cr(pcHClrT["r"],pcHClrT["g"],pcHClrT["b"], pcHClrT["a"] ), true, true, false, false)
-	-- Main UI Box
-	l_dRBEx(1,pcLocation["h"], pcLocation["v"] + pcSize['HH'], pcSize['BGW'], pcSize['BGH'], l_Cr(pcBGClrT["r"], pcBGClrT["g"], pcBGClrT["b"], pcBGClrT["a"] ), false, false, true, true)
-	-- Main Text - The Prop Count
-	l_dSTt( l_LP():GetCount( "props" ) or 0, "PropCounterText", ScrW() * 0.035, ScrH() * 0.0546,l_Cr(pcBGTClrT["r"], pcBGTClrT["g"], pcBGTClrT["b"], pcBGTClrT["a"] ), 1, 1,TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER)
-	-- Header Text - Says "Prop Count"
-	l_dSTt( "Prop Count", "PropCounterHeadText", ScrW() * 0.035, ScrH() * .0295,l_Cr(pcHTClrT["r"], pcHTClrT["g"], pcHTClrT["b"], pcHTClrT["a"] ), 1, 1,TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER)
-  end)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-l_hAd("OnScreenSizeChanged", "PropCounterAdjustSize",function(ScW,ScH) 
+hook.Add("OnScreenSizeChanged", "PropCounterAdjustSize",function() 
+	local oldScW = ScW
+	local oldScH = ScH
 	ScW = ScrW() 
 	ScH = ScrH()
+	print("Larry's Prop Counter: Screen size has changed from " .. oldScW .. 'x' .. oldScH .. ' to ' .. ScW .. 'x' .. ScH)
+	FontSize = (((ScH *.03)+(ScW *.04))/4) * scale
+	surface.CreateFont( "PropCounterText", {
+		font = "Arial",
+		extended = false,
+		size = (((ScH *.03)+(ScW *.04))/4) * scale,
+		weight = 600,
+		antialias = true,
+	})
+	hook.Remove( "HUDPaint", "ClientPropCounterHUDHook" )
+	timer.Simple( 1, function(ScW,ScH) 
+		hook.Add("HUDPaint", "ClientPropCounterHUDHook", function(ScW,ScH) pcHUDH() pcHUDBG() end)
+	end)
 end)
 
 
 
-
+local cvars_AddChangeCallback = (CLIENT and cvars.AddChangeCallback or NULL)
 -- Prop Counter Header Color R/G/B/A
-l_cACCk("lpc_HClr_r", function(convar_name,value_old,value_new) if value_new == value_old then return end pcHClrT["r"] = value_new end)
-l_cACCk("lpc_HClr_g", function(convar_name,value_old,value_new) if value_new == value_old then return end pcHClrT["g"] = value_new end)
-l_cACCk("lpc_HClr_b", function(convar_name,value_old,value_new) if value_new == value_old then return end pcHClrT["b"] = value_new end)
-l_cACCk("lpc_HClr_a", function(convar_name,value_old,value_new) if value_new == value_old then return end pcHClrT["a"] = value_new end)
+cvars_AddChangeCallback("lpc_HClr_r", function(convar_name,value_old,value_new) if value_new == value_old then return end hr = value_new end)
+cvars_AddChangeCallback("lpc_HClr_g", function(convar_name,value_old,value_new) if value_new == value_old then return end hg = value_new end)
+cvars_AddChangeCallback("lpc_HClr_b", function(convar_name,value_old,value_new) if value_new == value_old then return end hb = value_new end)
+cvars_AddChangeCallback("lpc_HClr_a", function(convar_name,value_old,value_new) if value_new == value_old then return end ha = value_new end)
 -- Prop Counter Background Color R/G/B/A
-l_cACCk("lpc_BGClr_r", function(convar_name,value_old,value_new) if value_new == value_old then return end pcBGClrT["r"] = value_new end)
-l_cACCk("lpc_BGClr_g", function(convar_name,value_old,value_new) if value_new == value_old then return end pcBGClrT["g"] = value_new end)
-l_cACCk("lpc_BGClr_b", function(convar_name,value_old,value_new) if value_new == value_old then return end pcBGClrT["b"] = value_new end)
-l_cACCk("lpc_BGClr_a", function(convar_name,value_old,value_new) if value_new == value_old then return end pcBGClrT["a"] = value_new end)
+cvars_AddChangeCallback("lpc_BGClr_r", function(convar_name,value_old,value_new) if value_new == value_old then return end bgr = value_new end)
+cvars_AddChangeCallback("lpc_BGClr_g", function(convar_name,value_old,value_new) if value_new == value_old then return end bgg = value_new end)
+cvars_AddChangeCallback("lpc_BGClr_b", function(convar_name,value_old,value_new) if value_new == value_old then return end bgb = value_new end)
+cvars_AddChangeCallback("lpc_BGClr_a", function(convar_name,value_old,value_new) if value_new == value_old then return end bga = value_new end)
 -- Prop Counter 'Prop Count' Text Color R/G/B/A
-l_cACCk("lpc_HTClr_r", function(convar_name,value_old,value_new) if value_new == value_old then return end pcHTClrT["r"] = value_new end)
-l_cACCk("lpc_HTClr_g", function(convar_name,value_old,value_new) if value_new == value_old then return end pcHTClrT["g"] = value_new end)
-l_cACCk("lpc_HTClr_b", function(convar_name,value_old,value_new) if value_new == value_old then return end pcHTClrT["b"] = value_new end)
-l_cACCk("lpc_HTClr_a", function(convar_name,value_old,value_new) if value_new == value_old then return end pcHTClrT["a"] = value_new end)
+cvars_AddChangeCallback("lpc_HTClr_r", function(convar_name,value_old,value_new) if value_new == value_old then return end htr = value_new end)
+cvars_AddChangeCallback("lpc_HTClr_g", function(convar_name,value_old,value_new) if value_new == value_old then return end htg = value_new end)
+cvars_AddChangeCallback("lpc_HTClr_b", function(convar_name,value_old,value_new) if value_new == value_old then return end htb = value_new end)
+cvars_AddChangeCallback("lpc_HTClr_a", function(convar_name,value_old,value_new) if value_new == value_old then return end hta = value_new end)
 -- Prop Counter Count Text Color R/G/B/A
-l_cACCk("lpc_BGTClr_r", function(convar_name,value_old,value_new) if value_new == value_old then return end pcBGTClrT["r"] = value_new end)
-l_cACCk("lpc_BGTClr_g", function(convar_name,value_old,value_new) if value_new == value_old then return end pcBGTClrT["g"] = value_new end)
-l_cACCk("lpc_BGTClr_b", function(convar_name,value_old,value_new) if value_new == value_old then return end pcBGTClrT["b"] = value_new end)
-l_cACCk("lpc_BGTClr_a", function(convar_name,value_old,value_new) if value_new == value_old then return end pcBGTClrT["a"] = value_new end)
+cvars_AddChangeCallback("lpc_BGTClr_r", function(convar_name,value_old,value_new) if value_new == value_old then return end bgtr = value_new end)
+cvars_AddChangeCallback("lpc_BGTClr_g", function(convar_name,value_old,value_new) if value_new == value_old then return end bgtg = value_new end)
+cvars_AddChangeCallback("lpc_BGTClr_b", function(convar_name,value_old,value_new) if value_new == value_old then return end bgtb = value_new end)
+cvars_AddChangeCallback("lpc_BGTClr_a", function(convar_name,value_old,value_new) if value_new == value_old then return end bgta = value_new end)
 -- Prop Counter Count Text Color R/G/B/A
-l_cACCk("lpc_vertical", function(convar_name,value_old,value_new) if value_new == value_old then return end pcLocation["v"] = value_new end)
-l_cACCk("lpc_horizontal", function(convar_name,value_old,value_new) if value_new == value_old then return end pcLocation["h"] = value_new end)
+cvars_AddChangeCallback("lpc_vertical", function(convar_name,value_old,value_new) if value_new == value_old then return end vLocation = value_new end)
+cvars_AddChangeCallback("lpc_horizontal", function(convar_name,value_old,value_new) if value_new == value_old then return end hLocation = value_new end)
+cvars_AddChangeCallback("lpc_scale", function(convar_name,value_old,value_new) if value_new == value_old then return end scale = value_new 
+	FontSize = (((ScH *.03)+(ScW *.04))/4) * scale
+	surface.CreateFont( "PropCounterText", {font = "Arial",extended = false,size = (((ScH *.03)+(ScW *.04))/4) * scale,weight = 600,antialias = true,})
+	hook.Remove( "HUDPaint", "ClientPropCounterHUDHook" )
+	timer.Simple( 1, function(ScW,ScH) hook.Add("HUDPaint", "ClientPropCounterHUDHook", function(ScW,ScH)pcHUDH()pcHUDBG()end)end)
+end)
+cvars_AddChangeCallback("lpc_enabled", function(convar_name,value_old,value_new)  
+	if value_new == value_old then return end
+	if value_new == '1' then timer.Simple( 1, function(ScW,ScH) hook.Add("HUDPaint", "ClientPropCounterHUDHook", function(ScW,ScH)pcHUDH()pcHUDBG()end)end)
+	elseif value_new == '0' then hook.Remove( "HUDPaint", "ClientPropCounterHUDHook" )
+	end
+end)
