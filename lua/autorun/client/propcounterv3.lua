8a88cb9abe40fbe71ea95c768cb68a975f7c44e5
QuirkyLarry <larrytmoore1@gmail.com>
2023-07-10 20:58:49 -0500

    Formatting update

--- lua/autorun/client/propcounterv3.lua
+++ lua/autorun/client/propcounterv3.lua
@@ -1,13 +1,14 @@
-local Create_CCV = (CLIENT and CreateClientConVar or NILL)
-local Get_CV = (CLIENT and GetConVar or NILL)
-local hook_A = (CLIENT and hook.Add or NILL)
-local cvars_ACC = (CLIENT and cvars.AddChangeCallback or NILL)
-local surface_SDC = (CLIENT and surface.SetDrawColor or NILL)
-local surface_DR = (CLIENT and surface.DrawRect or NILL)
-local surface_STC = (CLIENT and surface.SetTextColor or NILL)
-local surface_STP = (CLIENT and surface.SetTextPos or NILL)
-local surface_SF = (CLIENT and surface.SetFont or NILL)
-local surface_DT = (CLIENT and surface.DrawText or NILL)
+local Create_CCV = CreateClientConVar
+local Get_CV = GetConVar
+local hook_A = hook.Add
+local cvars_ACC = cvars.AddChangeCallback
+local surface_SDC = surface.SetDrawColor
+local surface_DR = surface.DrawRect
+local surface_STC = surface.SetTextColor
+local surface_STP = surface.SetTextPos
+local surface_SF = surface.SetFont
+local surface_DT = surface.DrawText
+local ScW, ScH = ScrW(),ScrH()
 Create_CCV("lpc_HClr_r", 0, true, false, "", 0, 255 )
 Create_CCV("lpc_HClr_g", 191, true, false, "", 0, 255 )
 Create_CCV("lpc_HClr_b", 255, true, false, "", 0, 255 )
@@ -30,7 +31,6 @@ Create_CCV("lpc_barf", 1, true, false, "", 0, 1)
 
 local rainbowBarf = Get_CV("lpc_barf"):GetBool()
 local propScale = Get_CV("lpc_scale"):GetInt()
-local ScW, ScH = ScrW(),ScrH()
 local vLocation, hLocation = Get_CV("lpc_vertical"):GetInt(),Get_CV("lpc_horizontal"):GetInt()
 local hr,hg,hb,ha = Get_CV("lpc_HClr_r"):GetInt(),Get_CV("lpc_HClr_g"):GetInt(),Get_CV("lpc_HClr_b"):GetInt(),Get_CV("lpc_HClr_a"):GetInt()
 local htr,htg,htb = Get_CV("lpc_HTClr_r"):GetInt(),Get_CV("lpc_HTClr_g"):GetInt(),Get_CV("lpc_HTClr_b"):GetInt()
@@ -40,7 +40,7 @@ local bgtr,bgtg,bgtb = Get_CV("lpc_BGTClr_r"):GetInt(),Get_CV("lpc_BGTClr_g"):Ge
 surface.CreateFont( "LarrysPropCounterText", {
 	font = "Arial",
 	extended = false,
-	size = (ScW *.015) * propScale,
+	size = (ScW * .015) * propScale,
 	weight = 600,
 	antialias = true,
 })
@@ -52,7 +52,7 @@ end )
 
 -- Creates Larry's Prop Counter Sub-Category
 hook_A( "PopulateToolMenu", "PropCounterSettings", function()
-	spawnmenu.AddToolMenuOption( "Utilities", "#Larry's Prop Counter", "PCCM", "#Prop Counter Color", "", "", function( ColorWheelPanel ) 
+	spawnmenu.AddToolMenuOption( "Utilities", "#Larry's Prop Counter", "PCCM", "#Prop Counter Color", "", "", function(ColorWheelPanel)
 		ColorWheelPanel:ColorPicker("Prop Counter Header Color", "lpc_HClr_r", "lpc_HClr_g", "lpc_HClr_b", "lpc_HClr_a")
 		ColorWheelPanel:ColorPicker("Prop Counter Background Color", "lpc_BGClr_r", "lpc_BGClr_g", "lpc_BGClr_b", "lpc_BGClr_a" )
 		ColorWheelPanel:ColorPicker("'Prop Count' Text Color", "lpc_HTClr_r", "lpc_HTClr_g", "lpc_HTClr_b", NILL)
@@ -72,37 +72,36 @@ local pcHUD = function()
 	local tw, th = surface.GetTextSize(pcpropText)
 	local ptw, pth = surface.GetTextSize(tostring(count))
 	if rainbowBarf then surface_SDC(hr,hg,hb,ha) else surface_SDC(HSVToColor(  ( CurTime() * 400 ) % 360, 1, 1 )) end
-	surface_DR(hLocation, vLocation, tw+5, th+3)
+	surface_DR(hLocation, vLocation, tw + 5, th + 3)
 	surface_STC(htr,htg,htb)
-	surface_STP(hLocation+1, vLocation+1.5)
+	surface_STP(hLocation + 1, vLocation + 1.5)
 	surface_DT(pcpropText)
 	surface_SDC(bgr, bgg, bgb, bga )
-	surface_DR(hLocation+tw, vLocation, ptw+3, pth+3)
+	surface_DR(hLocation + tw, vLocation, ptw + 3, pth + 3)
 	surface_STC(bgtr, bgtg, bgtb)
-	surface_STP(hLocation+tw+1, vLocation+2)
+	surface_STP(hLocation + tw + 1, vLocation + 2)
 	surface_DT(count)
 end
 -- Calls local HUD function and runs it.
-hook_A("HUDPaint", "PropCounterHUDHook2023", function(ScW,ScH) pcHUD() end)
+hook_A("HUDPaint", "PropCounterHUDHook2023", function() pcHUD() end)
 -- Gets new resolution, changes font size, removes and recreates HUD with new Font and HUD dimensions.
-hook_A("OnScreenSizeChanged", "PropCounterAdjustSize",function() 
+hook_A("OnScreenSizeChanged", "PropCounterAdjustSize",function()
 	local oldScW = ScW
 	local oldScH = ScH
-	ScW = ScrW() 
+	ScW = ScrW()
 	ScH = ScrH()
 	print("Larry's Prop Counter: Screen size has changed from " .. oldScW .. 'x' .. oldScH .. ' to ' .. ScW .. 'x' .. ScH)
 	surface.CreateFont( "LarrysPropCounterText", {
 		font = "Arial",
 		extended = false,
-		size = (ScW *.015) * propScale,
+		size = (ScW * .015) * propScale,
 		weight = 500,
 		antialias = true,
 	})
 	hook.Remove( "HUDPaint", "PropCounterHUDHook2023" )
-	timer.Simple( 1, function(ScW,ScH)  hook_A("HUDPaint", "PropCounterHUDHook2023", function(ScW,ScH) pcHUD() end) end)
+	timer.Simple( 1, function()  hook_A("HUDPaint", "PropCounterHUDHook2023", function() pcHUD() end) end)
 end)
 
-local cvars_ACC = (CLIENT and cvars.AddChangeCallback or NULL)
 -- Prop Counter Header Color R/G/B/A
 cvars_ACC("lpc_HClr_r", function(convar_name,value_old,value_new) if value_new == value_old then return end hr = value_new end)
 cvars_ACC("lpc_HClr_g", function(convar_name,value_old,value_new) if value_new == value_old then return end hg = value_new end)
@@ -124,14 +123,14 @@ cvars_ACC("lpc_BGTClr_b", function(convar_name,value_old,value_new) if value_new
 -- Prop Counter Count Text Color R/G/B/A
 cvars_ACC("lpc_vertical", function(convar_name,value_old,value_new) if value_new == value_old then return end vLocation = value_new end)
 cvars_ACC("lpc_horizontal", function(convar_name,value_old,value_new) if value_new == value_old then return end hLocation = value_new end)
-cvars_ACC("lpc_scale", function(convar_name,value_old,value_new) if value_new == value_old then return end propScale = value_new 
-	surface.CreateFont( "LarrysPropCounterText", {font = "Arial",extended = false,size = (ScW *.015) * propScale,weight = 600,antialias = true,})
+cvars_ACC("lpc_scale", function(convar_name,value_old,value_new) if value_new == value_old then return end propScale = value_new
+	surface.CreateFont( "LarrysPropCounterText", {font = "Arial",extended = false,size = (ScW * .015) * propScale,weight = 600,antialias = true,})
 	hook.Remove( "HUDPaint", "PropCounterHUDHook2023" )
-	timer.Simple( 1, function(ScW,ScH) hook_A("HUDPaint", "PropCounterHUDHook2023", function(ScW,ScH)pcHUD()end)end)--]]
+	timer.Simple( 1, function() hook_A("HUDPaint", "PropCounterHUDHook2023", function() pcHUD() end) end) --]]
 end)
-cvars_ACC("lpc_enabled", function(convar_name,value_old,value_new)  
+cvars_ACC("lpc_enabled", function(convar_name,value_old,value_new)
 	if value_new == value_old then return end
-	if value_new == '1' then timer.Simple( 1, function(ScW,ScH) hook_A("HUDPaint", "PropCounterHUDHook2023", function(ScW,ScH)pcHUD()end)end)
+	if value_new == '1' then timer.Simple( 1, function() hook_A("HUDPaint", "PropCounterHUDHook2023", function() pcHUD() end) end)
 	elseif value_new == '0' then hook.Remove( "HUDPaint", "PropCounterHUDHook2023" )
 	end
 end)
